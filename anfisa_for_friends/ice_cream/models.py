364f5bc4378158ddca94ee1fa7a501c32c84f5bb
Max Gulin <gulin_1371@mail.ru>
2026-03-17 15:56:41 +0700

    git

--- anfisa_for_friends/ice_cream/models.py
+++ anfisa_for_friends/ice_cream/models.py
@@ -1,11 +1,1 @@
 from django.db import models
-from core.models import PublishedModel
-
-class Category(PublishedModel):
-    title = models.CharField(max_length=256)
-    slug = models.SlugField(max_length=64, unique=True)
-    output_order = models.PositiveSmallIntegerField(default=100)
-    
-    class Meta:
-        verbose_name = 'категория'
-        verbose_name_plural = 'Категории'
